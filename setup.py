30fb9952263a243c3f6b64abc7b580697e874dcc
CSimmo <chris_simpkin_1@msn.com>
2020-10-21 17:27:47 +0100

    requirments.txt, setup.py and vesion info added to __init__.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+import pathlib
+from setuptools import find_packages, setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="vsapy",
+    version="0.1.0",
+    description="Vector Symbolic Architecture(VSA) library that allows building VSA apps that use variousl flavours of VSA vectors",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/vsapy/vsapy",
+    author="Chris Simpkin",
+    author_email="simpkin.chris@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=find_packages(exclude=("tests",)),
+    include_package_data=False,
+    install_requires=["numpy", "scipy"],
+    entry_points={},
+)
