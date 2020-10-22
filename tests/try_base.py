7d57c6563e91e304b323b3808d51f3d330a9adbb
CSimmo <chris_simpkin_1@msn.com>
2020-10-22 12:40:34 +0100

    Update to try_base.py simply object naming using imports

--- tests/try_base.py
+++ tests/try_base.py
@@ -1,13 +1,14 @@
 
 import vsapy as vsa
+from vsapy.vsatype import VsaType, VsaBase
 import numpy as np
 
 
 print("\n============ Test creating a VsaType from numpy generator =====================")
 vd = 5  # vector dimension
 print("Creating vec of each type, lenth={}, filled with zeros".format(vd))
-for vt in vsa.VsaType:
-    v = vsa.VsaBase(np.zeros(vd), vsa_type=vt)
+for vt in VsaType:
+    v = VsaBase(np.zeros(vd), vsa_type=vt)
     print("\t{}: Len={}, dtype={}, {}".format(vt.name, len(v), v.dtype, v))
 
 
@@ -15,10 +16,10 @@ for vt in vsa.VsaType:
 print("\n============ Test VsaType Conversions =====================")
 trunc_view = 10
 vd = 1000  # vector dimension
-for vt in vsa.VsaType:
+for vt in VsaType:
     print()
     v1 = vsa.randvec(vd, vsa_type=vt)
-    for vtt in vsa.VsaType:
+    for vtt in VsaType:
         try:
             t1 = vsa.to_vsa_type(v1, vsa_type=vtt)
             print("Convert {} to {} = \n\t\t{}\n\t\t{}".format(v1.vsatype.name, t1.vsatype.name, v1[:trunc_view], t1[:trunc_view]))
@@ -29,7 +30,7 @@ for vt in vsa.VsaType:
 # Test binding
 print("\n============ Test BIND / UNBIND =====================")
 vd=2048  # vector dimension
-for vt in vsa.VsaType:
+for vt in VsaType:
     v1 = vsa.randvec(vd, vsa_type=vt)
     print("\n{}: compare self = {:0.4f}".format(vt.name, vsa.hsim(v1, v1)))
     v2 = vsa.randvec(vd, vsa_type=vt)
@@ -45,16 +46,16 @@ for vt in vsa.VsaType:
 print("\n===================== Test Simple Concept / UNBIND =====================")
 vd=2048 # vector dimension
 num_vecs = 10
-for vt in vsa.VsaType:
+for vt in VsaType:
     vecs = vsa.randvec((num_vecs, vd), vsa_type=vt)
     bag_vec = np.sum(vecs, axis=0)
     bag_vec = vsa.normalize(bag_vec, seqlength=len(vecs))
     i = 0
-    if vt != vsa.VsaType.HRR:
+    if vt != VsaType.HRR:
         expected_hd = "<--> {:0.4f}=expected".format(1.0 - vsa.get_hd_threshold(num_vecs))
     else:
         # Here we see calc expected_hd of an orthogonal vector because we don't know how to calc expected_hd
-        expected_hd = "> {:0.4f} orthogonal vector".format(vsa.hsim(vsa.randvec(vd, vsa_type=vsa.VsaType.HRR), bag_vec))
+        expected_hd = "> {:0.4f} orthogonal vector".format(vsa.hsim(vsa.randvec(vd, vsa_type=VsaType.HRR), bag_vec))
 
     print("{}:Binding {} sub-vectors into 'bag_vec'".format(vt.name, num_vecs))
     for v in vecs:
